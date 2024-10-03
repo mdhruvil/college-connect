dbac769c68b36a7e780adfe2a60e20e8471e059f
mdhruvil <dhruvil1808@gmail.com>
2024-10-03 10:24:43 +0530

    fix: club page skeleton

--- src/app/(app)/clubs/[clubId]/page.tsx
+++ src/app/(app)/clubs/[clubId]/page.tsx
@@ -8,6 +8,7 @@ import { Button } from "~/components/ui/button";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "~/components/ui/tabs";
 import { getInitials } from "~/lib/utils";
 import { api } from "~/trpc/react";
+import { Skeleton } from "~/components/ui/skeleton";
 
 export default function ClubPage({ params }: { params: { clubId: string } }) {
   const {
@@ -22,18 +23,22 @@ export default function ClubPage({ params }: { params: { clubId: string } }) {
   const leaveClub = api.club.leaveClub.useMutation();
 
   if (isLoading) {
-    return <div>Loading...</div>;
+    return (
+      <div className="container mx-auto h-screen max-w-md space-y-4 !bg-white px-4 py-8">
+        <ClubPageSkeleton />
+      </div>
+    );
   }
   if (error || !data) {
     return (
-      <div className="container mx-auto max-w-md space-y-4 px-4 py-8">
+      <div className="container mx-auto max-w-md space-y-4 !bg-white px-4 py-8">
         <ErrorWithLogin errorMsg={error?.message ?? "Something went wrong"} />
       </div>
     );
   }
 
   return (
-    <div className="container mx-auto max-w-md space-y-4 px-4 py-8">
+    <div className="container mx-auto h-screen max-w-md space-y-4 !bg-white px-4 py-8">
       <div className="flex flex-col items-center">
         <Image
           src={data.image ?? ""}
@@ -150,4 +155,30 @@ enum ClubTabs {
   About = "About",
   Events = "Events",
   Members = "Members",
+}
+
+function ClubPageSkeleton() {
+  return (
+    <>
+      <div className="flex flex-col items-center">
+        <Skeleton className="h-[100px] w-[100px] rounded-2xl" />
+        <Skeleton className="mt-4 h-8 w-48" />
+        <div className="mt-3 flex items-center gap-10">
+          <div>
+            <Skeleton className="h-7 w-16" />
+            <Skeleton className="mt-1 h-4 w-12" />
+          </div>
+          <div>
+            <Skeleton className="h-7 w-16" />
+            <Skeleton className="mt-1 h-4 w-12" />
+          </div>
+        </div>
+        <Skeleton className="mt-4 h-10 w-32" />
+      </div>
+      <div className="mt-4">
+        <Skeleton className="h-10 w-full" />
+        <Skeleton className="mt-4 h-32 w-full" />
+      </div>
+    </>
+  );
 }
